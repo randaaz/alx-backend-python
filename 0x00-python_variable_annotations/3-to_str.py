a9b0b57d14190701e2f00fcd2727c8221a6c1494
randaaz <randaaziz972@gmail.com>
2024-06-05 21:26:30 -0700

    This script defines a function to convert a floating-point number to a string using type annotations.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function to convert a floating-point number to a string  """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the input number. """
+    return str(n)
