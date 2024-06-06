cb7a7e6945979d30b954595d1e9e814132add776
randaaz <randaaziz972@gmail.com>
2024-06-05 21:17:00 -0700

    Adds two numbers together

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables in Python.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Adds two numbers together
+    '''
+    return a + b
