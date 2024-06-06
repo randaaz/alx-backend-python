b1818dccbcce5c378cdb17b2c4b1264ff444f2a7
randaaz <randaaziz972@gmail.com>
2024-06-05 21:22:39 -0700

    function to compute the floor of a floating-point number using type annotations.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" function to compute the floor of a floating-point number"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """ Returns The largest integer less than or equal to the input number. """
+    return math.floor(n)
