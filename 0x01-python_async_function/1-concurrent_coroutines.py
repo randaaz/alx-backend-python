89a7996e09cbdf529ea0d480030a43897afbfb02
randaaz <randaaziz972@gmail.com>
2024-06-09 23:05:39 -0700

    Wait for n random delays

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Asynchronous function that waits for multiple random delays. """
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Wait for n random delays and return the list of durations.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
