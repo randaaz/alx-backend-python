8e63135646dd128fbf464c3ba481a59daeab72fd
randaaz <randaaziz972@gmail.com>
2024-06-28 03:29:33 -0700

    modify

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A github org client
+"""This module provides a client
 """
 from typing import (
     List,
@@ -14,31 +14,31 @@ from utils import (
 
 
 class GithubOrgClient:
-    """A Githib org client
+    """A client for GitHub organization data.
     """
     ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
-        """Init method of GithubOrgClient"""
+        """Initialize the client with the organization name."""
         self._org_name = org_name
 
     @memoize
     def org(self) -> Dict:
-        """Memoize org"""
+        """Get organization data from GitHub."""
         return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
-        """Public repos URL"""
+        """Get the URL for the public repositories of the organization"""
         return self.org["repos_url"]
 
     @memoize
     def repos_payload(self) -> Dict:
-        """Memoize repos payload"""
+        """Get the payload of the public repositories."""
         return get_json(self._public_repos_url)
 
     def public_repos(self, license: str = None) -> List[str]:
-        """Public repos"""
+        """Get a list of public repositories"""
         json_payload = self.repos_payload
         public_repos = [
             repo["name"] for repo in json_payload
@@ -49,7 +49,7 @@ class GithubOrgClient:
 
     @staticmethod
     def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
-        """Static: has_license"""
+        """"Check if a repository has a specific license."""
         assert license_key is not None, "license_key cannot be None"
         try:
             has_license = access_nested_map(repo, ("license", "key")) == license_key
